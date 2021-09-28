35ee2aa7fa928c38f43d0afe41252741565dd877
Sergio Almeida <sergioalmeidaa00@gmail.com>
2021-09-28 12:01:08 -0300

    Desafio Codelandia

---
+++ assets/js/main.js
@@ -0,0 +1,60 @@
+const btnMobile = document.querySelector('#btn-mobile');
+const ulNav = document.querySelector('nav ul');
+const links = document.querySelectorAll('nav ul li a')
+
+// Ao clicar no item do menu, escode o menu
+for (const link of links) {
+    link.addEventListener('click', () => {
+        ulNav.classList.remove('active');
+    });
+}
+// BTN PARA VOLTAR AO TOPO
+function topoScroll() {
+    const btnTopo = document.querySelector('.topo');
+    if (window.scrollY >= 500) {
+        btnTopo.classList.add('active');
+    } else {
+        btnTopo.classList.remove('active');
+    }
+}
+// MUDA A COR DA NAV AO ROLAR O SCROLL;
+function headerScroll() {
+    const header = document.querySelector('header');
+    const navHeight = header.offsetHeight;
+    if (window.scrollY >= navHeight) {
+        header.classList.add('scroll')
+    } else {
+        header.classList.remove('scroll');
+        header.getElementsByClassName.color = 'red';
+    }
+}
+// ADICIONA OU REMOVE A CLASS ACTIVE 
+function toggleMenu() {
+    const btNav = document.querySelector('.menu-nav ul');
+    btNav.classList.toggle('active');
+}
+btnMobile.addEventListener('click', toggleMenu);
+
+// API DE AIMAÇÃO DA ScrollReveal, EFEITO NO SCROLL AO ROLAR
+const scrollReveal = ScrollReveal({
+    origin: 'left',
+    distance: '50px',
+    delay: 375,
+    duration: 700,
+    // reset: true,
+});
+scrollReveal.reveal(`
+    .introducao-info, .introducao-img,
+    #sobre .title, #sobre .sobre-conteudo,
+    #projetos .title, #projetos .card,
+    #servicos .title, #servicos .card,
+    #skills .title, #skills 
+`, { interval: 100 });
+
+
+
+// FICA ESCUTANDO O EVENTO DE SCROLL E CHAMA AS FUNÇÕES 
+window.addEventListener('scroll', () => {
+    headerScroll();
+    topoScroll();
+})
